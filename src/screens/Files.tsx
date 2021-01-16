3efca8b292626f50a73677931c043ea3865b543a
Harshal Sanghvi <harshal2030@outlook.com>
2021-01-16 23:04:46 +0530

    created file screen

--- src/screens/Files.tsx
+++ src/screens/Files.tsx
@@ -1,14 +1,190 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import axios from 'axios';
+import {View, Text, ActivityIndicator, StyleSheet} from 'react-native';
+import {Header} from 'react-native-elements';
+import {StackNavigationProp} from '@react-navigation/stack';
+import {RouteProp} from '@react-navigation/native';
+import {connect} from 'react-redux';
+import {FlatGrid} from 'react-native-super-grid';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-class Files extends React.Component {
+import {StoreState} from '../global';
+import {Class} from '../global/actions/classes';
+
+import {RootStackParamList} from '../navigators/types';
+import {moduleUrl} from '../utils/urls';
+import {ContainerStyles} from '../styles/styles';
+import {commonBlue, commonGrey} from '../styles/colors';
+import {
+  imageExtPattern,
+  videoExtPattern,
+  pdfExtPattern,
+  pptExtPattern,
+  docExtPattern,
+  excelExtPattern,
+} from '../utils/regexPatterns';
+
+interface Props {
+  navigation: StackNavigationProp<RootStackParamList, 'Files'>;
+  currentClass: Class;
+  token: string;
+  route: RouteProp<RootStackParamList, 'Files'>;
+}
+
+interface File {
+  title: string;
+  id: string;
+  filename: string;
+}
+
+interface State {
+  files: File[];
+  loading: boolean;
+  errored: boolean;
+}
+
+class Files extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      files: [],
+      loading: true,
+      errored: false,
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get<File[]>(
+        `${moduleUrl}/${this.props.currentClass.id}/${this.props.route.params.moduleId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${this.props.token}`,
+          },
+        },
+      )
+      .then((res) => {
+        this.setState({files: res.data, loading: false});
+      })
+      .catch(() => this.setState({errored: true, loading: false}));
+  }
+
+  renderFileBlock = ({item}: {item: File}) => {
+    let name: string = 'image';
+
+    if (item.filename.match(imageExtPattern)) {
+      name = 'image';
+    }
+
+    if (item.filename.match(videoExtPattern)) {
+      name = 'play-box';
+    }
+
+    if (item.filename.match(pdfExtPattern)) {
+      name = 'pdf-box';
+    }
+
+    if (item.filename.match(docExtPattern)) {
+      name = 'microsoft-word';
+    }
+
+    if (item.filename.match(pptExtPattern)) {
+      name = 'microsoft-powerpoint';
+    }
+
+    if (item.filename.match(excelExtPattern)) {
+      name = 'microsoft-excel';
+    }
+
+    return (
+      <View style={styles.fileBlock}>
+        <MaterialCommunityIcons name={name} size={30} color={commonGrey} />
+        <Text style={styles.fileBlockText}>{item.title}</Text>
+      </View>
+    );
+  };
+
+  renderContent = () => {
+    const {loading, errored, files} = this.state;
+
+    if (loading) {
+      return (
+        <View style={ContainerStyles.centerElements}>
+          <ActivityIndicator animating size="large" color={commonBlue} />
+        </View>
+      );
+    }
+
+    if (errored) {
+      return (
+        <View style={ContainerStyles.centerElements}>
+          <Text>
+            We're having trouble in fetching latest data. Please try again later
+          </Text>
+        </View>
+      );
+    }
+
+    if (files.length === 0) {
+      return (
+        <View>
+          <Text>Wow! Such empty</Text>
+        </View>
+      );
+    }
+
+    return (
+      <FlatGrid
+        data={files}
+        spacing={6}
+        keyExtractor={(item) => item.id}
+        renderItem={this.renderFileBlock}
+      />
+    );
+  };
+
   render() {
     return (
       <View>
-        <Text>Files</Text>
+        <Header
+          centerComponent={{
+            text: 'Files',
+            style: {fontSize: 24, color: '#fff', fontWeight: '600'},
+          }}
+          leftComponent={{
+            icon: 'arrow-back',
+            color: '#ffff',
+            size: 26,
+            onPress: this.props.navigation.goBack,
+          }}
+        />
+        {this.renderContent()}
       </View>
     );
   }
 }
 
-export default Files;
+const styles = StyleSheet.create({
+  fileBlock: {
+    borderWidth: 1,
+    borderColor: commonGrey,
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  fileBlockText: {
+    marginTop: 5,
+    fontSize: 16,
+    fontWeight: '600',
+  },
+});
+
+const mapStateToProps = (state: StoreState) => {
+  return {
+    token: state.token!,
+    currentClass: state.currentClass!,
+  };
+};
+
+export default connect(mapStateToProps)(Files);
--- src/utils/regexPatterns.ts
+++ src/utils/regexPatterns.ts
@@ -1,3 +1,18 @@
 const usernamePattern = /^[A-Za-z]+[A-Za-z0-9_.]+$/;
 
-export {usernamePattern};
+const imageExtPattern = /\.(png|jpg|jpeg|gif)$/i;
+const videoExtPattern = /\.(mp4|mkv|mov|wmv)/i;
+const pdfExtPattern = /\.(pdf)$/i;
+const docExtPattern = /\.(doc|docx)$/i;
+const excelExtPattern = /\.(xlsx|xls)$/i;
+const pptExtPattern = /\.(ppt|pptx)$/i;
+
+export {
+  usernamePattern,
+  imageExtPattern,
+  videoExtPattern,
+  pdfExtPattern,
+  docExtPattern,
+  excelExtPattern,
+  pptExtPattern,
+};
